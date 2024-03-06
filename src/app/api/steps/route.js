d7690509804dff5cefc249e4d8f23a7f03f87874
Nicolas BREANT <aces.nico@gmail.com>
2024-03-06 22:21:54 +0100

    feat: CRUD Steps

---
+++ src/app/api/steps/route.js
@@ -0,0 +1,33 @@
+import prisma from "@/utils/db";
+import {NextResponse} from "next/server";
+import dayjs from "dayjs";
+
+export async function GET() {
+    try {
+        const steps = await prisma.step.findMany();
+        return NextResponse.json({data: steps}, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({error}, { status: 500 });
+    }
+}
+
+export async function POST(req) {
+    const { user_id, step_count, challenge_id } = await req.json();
+    const date = dayjs().toISOString();
+
+    try {
+        const step = await prisma.step.create({
+            data: {
+                user_id: user_id,
+                date: date,
+                step_count: step_count,
+                challenge_id: challenge_id,
+            },
+        });
+
+        return NextResponse.json({data: step}, { status: 201 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({error}, { status: 500 });
+    }
+}
