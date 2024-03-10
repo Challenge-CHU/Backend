d25ba6c6531b0e050e1718f988aa47e55376c891
Alan Courtois <alan@wesur.fr>
2024-03-10 23:59:19 +0100

    CRUD Challenges

---
+++ src/app/api/challenges/[id]/route.js
@@ -0,0 +1,67 @@
+import prisma from "@/utils/db";
+import { NextResponse} from "next/server";
+
+export async function GET(req, { params }) {
+    const challengeId = params.id;
+
+    if (!challengeId) {
+        return NextResponse.json({ error: "Missing challenge ID" }, { status: 400 });
+    }
+    try {
+        const challenge = await prisma.challenge.findUnique({
+            where: { id: challengeId },
+        });
+
+        if (!challenge) {
+            return NextResponse.json({ error: "Challenge not found" }, { status: 404 });
+        }
+
+        return NextResponse.json({ data: challenge }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
+
+export async function PUT(req, { params }) {
+    const challengeId = params.id;
+
+    if (!challengeId) {
+        return NextResponse.json({ error: "Missing challenge ID" }, { status: 400 });
+    }
+
+    const { name, description, start_date, end_date } = await req.json();
+
+    try {
+        const challenge = await prisma.challenge.update({
+            where: { id: challengeId },
+            data: {
+                name,
+                description,
+                start_date: new Date(start_date).toISOString(),
+                end_date: new Date(end_date).toISOString(),
+            },
+        });
+
+        return NextResponse.json({ data: challenge }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 404 });
+    }
+}
+
+export async function DELETE(req, { params }) {
+    const challengeId = params.id;
+
+    if (!challengeId) {
+        return NextResponse.json({ error: "Missing challenge ID" }, { status: 400 });
+    }
+
+    try {
+        await prisma.challenge.delete({
+            where: { id: challengeId },
+        });
+
+        return NextResponse.json({ data: "Challenge deleted" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
