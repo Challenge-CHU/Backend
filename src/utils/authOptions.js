e775d2bfd9f376ac91b5cc891ddfc40e2f878d30
nbreant <nbreant@assit-evolution.com>
2024-03-11 12:44:00 +0100

    wip

--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -27,7 +27,50 @@ const AuthOptions = {
                     username: process.env.ADMIN_USERNAME,
                 };
             }
-        })
+        }),
+        {
+            id: "credentialsMobile",
+            name: "CredentialsMobile",
+            type: "credentials",
+            credentials: {
+                identifier: {label: "Identifier", type: "text"},
+                challenge_password: {label: "Challenge Password", type: "password"},
+            },
+            authorize: async (credentials) => {
+                if (!credentials?.challenge_password || !credentials?.identifier) {
+                    throw new Error("Invalid identifier or challenge password");
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        identifier: credentials.identifier,
+                    }
+                });
+
+                //TODO: possible modifications
+                const challenge = await prisma.challenge.findUnique({
+                    where: {
+                        password: credentials.challenge_password,
+                    }
+                })
+
+                if (!user) {
+                    throw new Error("Invalid identifier or challenge password");
+                }
+
+                if (!challenge) {
+                    throw new Error("Invalid identifier or challenge password");
+                }
+
+                return {
+                    id: user.id,
+                    identifier: user.identifier,
+
+                };
+
+
+            }
+        }
     ],
     database: process.env.DATABASE_URL,
     session: {
@@ -39,6 +82,7 @@ const AuthOptions = {
                 token.id = user.id;
                 token.email = user.email;
             }
+            console.log("token ===> ", token);
             return token;
         },
         async session(session, token) {
@@ -46,7 +90,10 @@ const AuthOptions = {
             session.user.email = token.email;
             return session;
         }
-    }
+    },
+    pages: {
+        signIn: "/connexion",
+    },
 }
 
 export default AuthOptions;
