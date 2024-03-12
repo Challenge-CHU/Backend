6fb409532206ebb3b3374ba913d19f4627e30c5b
nbreant <nbreant@assit-evolution.com>
2024-03-12 11:26:50 +0100

    feat: auth

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,40 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import * as jose from 'jose'
 
-export const config = {
-    matcher: ["/"],
-};
+const secret = new TextEncoder().encode(
+    process.env.JWT_SECRET
+)
+
+export async function middleware(req) {
+    if (req.nextUrl.pathname.startsWith('/api/auth/login')) {
+        return NextResponse.next();
+    } else if (req.nextUrl.pathname.startsWith('/api')) {
+        let token = req.headers.get('authorization');
+        if (token) {
+            try {
+                token = token.replace('Bearer ', '');
+                const decoded = await jose.jwtVerify(token, secret);
+                req.user = decoded;
+
+                return NextResponse.next();
+            } catch (error) {
+                return NextResponse.json({error: "Unauthorized"}, {status: 401});
+            }
+        } else {
+            return NextResponse.json({error: "Unauthorized"}, {status: 401});
+
+        }
+
+        return NextResponse.next();
+    } else {
+        return withAuth({
+            pages: {
+                signIn: "/connexion",
+            },
+        })(req, res);
+    }
+}
+
+
+
--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -1,7 +1,4 @@
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/utils/db";
-import bcrypt from "bcrypt";
-import { sign as jwtSign } from 'jsonwebtoken';
 
 const AuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
@@ -23,81 +20,17 @@ const AuthOptions = {
                     throw new Error("Invalid email or password");
                 }
 
-                return {
+                const user = {
                     id: 1,
                     name: process.env.ADMIN_USERNAME,
-                };
-            }
-        }),
-        {
-            id: "credentialsMobile",
-            name: "CredentialsMobile",
-            type: "credentials",
-            credentials: {
-                identifier: {label: "Identifier", type: "text"},
-                challenge_password: {label: "Challenge Password", type: "password"},
-            },
-            authorize: async (credentials) => {
-                if (!credentials?.challenge_password || !credentials?.identifier) {
-                    throw new Error("Invalid identifier or challenge password");
+                    email : ""
                 }
-
-                const user = await prisma.user.findUnique({
-                    where: {
-                        identifier: credentials.identifier,
-                    }
-                });
-
-                //TODO: possible modifications
-                const challenge = await prisma.challenge.findUnique({
-                    where: {
-                        password: credentials.challenge_password,
-                    }
-                })
-
-                if (!user) {
-                    throw new Error("Invalid identifier or challenge password");
-                }
-
-                if (!challenge) {
-                    throw new Error("Invalid identifier or challenge password");
-                }
+                console.log('user defini ====>', user);
 
                 return user;
-
-
             }
-        }
+        })
     ],
-    database: process.env.DATABASE_URL,
-    jwt: {
-        secret: process.env.JWT_SECRET,
-    },
-    callbacks: {
-        async jwt({token, user}) {
-            if (user) {
-                const encodedToken = jwtSign(
-                    {
-                        id: user.id,
-                        name: user.name,
-                    },
-                    process.env.JWT_SECRET,
-                    {
-                        expiresIn: "1h",
-                    }
-                );
-                token.jwt = encodedToken;
-                token.id = user.id;
-                token.name = user.name;
-            }
-            return token;
-        },
-        async session(session) {
-            console.log("session ===> ", session);
-            session.session.jwt = session.token.jwt;
-            return session.session;
-        },
-    },
     pages: {
         signIn: "/connexion",
     },
