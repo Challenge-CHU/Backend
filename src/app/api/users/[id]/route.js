cd9b6b993abda0db07cf1f74b282ddf7e07510eb
Alan Courtois <alan@wesur.fr>
2024-03-12 22:40:00 +0100

    remove older streak system

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -9,40 +9,12 @@ export async function GET(req, { params }) {
     }
     try {
         const user = await prisma.user.findUnique({
-            where: { id: userId },
-            include: {
-                Steps: {
-                    orderBy: { date: "desc" },
-                }
-            },
+            where: { id: userId }
         });
 
         if (!user) {
             return NextResponse.json({ error: "User not found" }, { status: 404 });
         }
-
-        let currentStreak = 0;
-        let lastStepDate = new Date();
-
-        for (let i = 0; i < user.Steps.length; i++) {
-            const step = user.Steps[i];
-
-            if (lastStepDate) {
-                const lastStep = new Date(lastStepDate);
-                const currentStep = new Date(step.date);
-
-                if (lastStep.getDate() - currentStep.getDate() === 1 && step.step_count >= 10000) {
-                    currentStreak++;
-                } else {
-                    break;
-                }
-            }
-
-            lastStepDate = step.date;
-        }
-
-        delete user.Steps;
-        user.currentStreak = currentStreak;
 
         return NextResponse.json({ data: user }, { status: 200 });
     } catch (error) {
